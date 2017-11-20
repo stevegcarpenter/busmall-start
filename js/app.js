e0ac0b73b7c217db7dbda428344e026ea190c2cf
Steven Carpenter <stevegcarpenter@gmail.com>
2017-11-20 13:21:40 -0800

    Added skeleton JavaScript code to implement logic.

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var allProducts = [];
+var productNames = ['boots', 'chair', 'scissors']; // TODO: see the pattern here, and what you need to fill in?
+
+function Product(name, path) {
+  // TODO: Build your constructor and necessary properties.
+}
+
+// TODO: Don't forget to build your objects. How can you do this withough having to write 14 lines of `new Product(., ., .)`?
+
+var productRank = {
+  // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
+  // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
+
+  getRandomIndex: function() {
+    // TODO: Hmm... what's going to happen here?
+  },
+
+  displayImages: function() {
+    // TODO: Hmm... what's going to happen here?
+  },
+
+  tallyClicks: function(elementId) {
+    // TODO: Hmm... what's going to happen here?
+  },
+
+  displayResults: function() {
+    // TODO: Hmm... what's going to happen here?
+  },
+
+  showButton: function() {
+    // TODO: Hmm... what's going to happen here?
+  },
+
+  onClick: function() {
+    // TODO: Hmm... what's going to happen here?
+};
+
+productRank.imageEls.addEventListener('click', productRank.onClick);
+productRank.displayImages();
